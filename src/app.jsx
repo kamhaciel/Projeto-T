ed9de7289d93b202d62c6265f2c17c080c6a0965
kamhaciel <104441151+kamhaciel@users.noreply.github.com>
2024-12-01 22:14:06 -0300

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import Board from "./components/board.jsx";
+import React from "react";
+import Game from "./components/game.jsx";
+
+export default function App() {
+  return <Game />;
+}
